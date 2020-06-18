d4d9b0919827cf54a178fed8ed87b209092dd3b5
Rebecca Schedler <rebecca.schedler@gmx.net>
2020-06-18 14:10:20 +0200

    first part advanced

--- Advanced/script.js
+++ Advanced/script.js
@@ -11,6 +11,8 @@ var __extends = (this && this.__extends) || (function () {
         d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
     };
 })();
+var motorBikes = [];
+var trucks = [];
 var Vehicles = /** @class */ (function () {
     function Vehicles(brand, year) {
         this.brand = "";
@@ -25,8 +27,8 @@ var Vehicles = /** @class */ (function () {
 }());
 var Vehicle1 = new Vehicles("Fiat", "2002");
 var Vehicle2 = new Vehicles("Suzuki", "2005");
-document.write(Vehicle1.showInfo());
-document.write(Vehicle2.showInfo());
+// document.write(Vehicle1.showInfo());
+// document.write(Vehicle2.showInfo());
 var MotorBike = /** @class */ (function (_super) {
     __extends(MotorBike, _super);
     function MotorBike(brand, year, type, km, seats, fuel) {
@@ -35,14 +37,38 @@ var MotorBike = /** @class */ (function (_super) {
         _this.km = km;
         _this.seats = seats;
         _this.fuel = fuel;
+        motorBikes.push(_this);
+        console.table(motorBikes);
         return _this;
     }
     MotorBike.prototype.showInfo = function () {
-        return _super.prototype.showInfo + " It's a " + this.type + " which has " + this.km + ", has " + this.seats + " and fueled with " + this.fuel + ".<br>";
+        return _super.prototype.showInfo.call(this) + " It's a " + this.type + " which has " + this.km + ", has " + this.seats + " and fueled with " + this.fuel + ".<br>";
     };
     return MotorBike;
 }(Vehicles));
-var MotorBike1 = new MotorBike("Fiat", "2002", "bike", "30000", "2", "super");
-var MotorBike2 = new MotorBike("Suzuki", "2010", "bike", "50000", "1", "diesel");
-document.write(Vehicle1.showInfo());
-document.write(Vehicle2.showInfo());
+var Truck = /** @class */ (function (_super) {
+    __extends(Truck, _super);
+    function Truck(brand, year, type, km, seats, fuel) {
+        var _this = _super.call(this, brand, year) || this;
+        _this.type = type;
+        _this.km = km;
+        _this.seats = seats;
+        _this.fuel = fuel;
+        trucks.push(_this);
+        return _this;
+    }
+    Truck.prototype.showInfo = function () {
+        return _super.prototype.showInfo.call(this) + " It's a " + this.type + " which has " + this.km + ", has " + this.seats + " and fueled with " + this.fuel + ".<br>";
+    };
+    return Truck;
+}(Vehicles));
+var motorBike1 = new MotorBike("Fiat", "2002", "bike", "30000", "2", "super");
+var motorBike2 = new MotorBike("Suzuki", "2010", "bike", "50000", "1", "diesel");
+var truck1 = new Truck("Man", "2020", "truck", "1000", "3", "diesel");
+var truck2 = new Truck("Skania", "2020", "truck", "1000", "3", "diesel");
+for (var i = 0; i < motorBikes.length; i++) {
+    document.getElementById("motorbike").innerHTML += motorBikes[i].showInfo();
+}
+for (var i = 0; i < trucks.length; i++) {
+    document.getElementById("truck").innerHTML += trucks[i].showInfo();
+}
--- Advanced/script.ts
+++ Advanced/script.ts
@@ -1,3 +1,7 @@
+let motorBikes:Array<any>=[];
+let trucks:Array<any>=[];
+
+
 class Vehicles{
 	brand="";
 	year="";
@@ -16,8 +20,8 @@ class Vehicles{
 var Vehicle1 = new Vehicles("Fiat", "2002");
 var Vehicle2 = new Vehicles("Suzuki", "2005");
 
-document.write(Vehicle1.showInfo());
-document.write(Vehicle2.showInfo());
+// document.write(Vehicle1.showInfo());
+// document.write(Vehicle2.showInfo());
 
 
 class MotorBike extends Vehicles{
@@ -29,16 +33,46 @@ class MotorBike extends Vehicles{
 		this.km = km;
 		this.seats = seats;
 		this.fuel = fuel;
+		motorBikes.push(this);
+		console.table(motorBikes);
+
 	}
 
 	showInfo(){
-		return `${super.showInfo} It's a ${this.type} which has ${this.km}, has ${this.seats} and fueled with ${this.fuel}.<br>`;
+		return `${super.showInfo()} It's a ${this.type} which has ${this.km}, has ${this.seats} and fueled with ${this.fuel}.<br>`;
 	}
 
 }
 
-var MotorBike1 = new MotorBike("Fiat", "2002", "bike", "30000", "2", "super");
-var MotorBike2 = new MotorBike("Suzuki", "2010", "bike", "50000", "1", "diesel");
+class Truck extends Vehicles{
+	type; km; seats; fuel;
 
-document.write(Vehicle1.showInfo());
-document.write(Vehicle2.showInfo());
+	constructor(brand, year, type, km, seats, fuel){
+		super(brand, year);
+		this.type = type;
+		this.km = km;
+		this.seats = seats;
+		this.fuel = fuel;
+		trucks.push(this);
+	}
+
+	showInfo(){
+		return `${super.showInfo()} It's a ${this.type} which has ${this.km}, has ${this.seats} and fueled with ${this.fuel}.<br>`;
+	}
+
+}
+var motorBike1 = new MotorBike("Fiat", "2002", "bike", "30000", "2", "super");
+var motorBike2 = new MotorBike("Suzuki", "2010", "bike", "50000", "1", "diesel");
+
+var truck1 = new Truck("Man", "2020", "truck", "1000", "3", "diesel");
+var truck2 = new Truck("Skania", "2020", "truck", "1000", "3", "diesel");
+
+for (let i = 0; i<motorBikes.length; i++) {
+		document.getElementById("motorbike").innerHTML+= motorBikes[i].showInfo();
+	
+}
+
+for (let i = 0; i<trucks.length; i++) {
+		document.getElementById("truck").innerHTML+= trucks[i].showInfo();
+	
+}
